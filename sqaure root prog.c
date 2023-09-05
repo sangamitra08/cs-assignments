dd3aebf498cf88d8a5bd5b412e3a81a18b7bf69e
sangamitra08 <143521669+sangamitra08@users.noreply.github.com>
2023-09-05 21:13:18 +0530

    Add files via upload

---
+++ sqaure root prog.c
@@ -0,0 +1,12 @@
+//progarm to calculate square root of a number//
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float a;
+    printf("Enter the value of a:");
+    scanf("%f",&a);
+    float b= sqrt(a);
+    printf("The square root of a is: %f",b);
+    return(0);
+
+}
